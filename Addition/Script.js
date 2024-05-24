536f0b2027a12bca903bf6b932d43206ea4cd6c7
ZayanKamran <ziyanffking@gmail.com>
2024-05-24 21:02:33 +0500

    Calculator

---
+++ Addition/Script.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Write Your First Number");
+let num2 = prompt("Write Nour Second Number");
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+let result = num1 + num2
+
+alert ("Answer is " + result)
